7223e43c24258d2d5c343451b954dba4523c40c8
Shelby Rothman <srothman@corelation.local>
2021-11-17 11:57:02 -0800

    control-flow type analysis

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,16 @@
 function startGame() {
     // starting a new game
+
+    let playerName: string = 'Shelby'
+    logPlayer(playerName);
+
     let messagesElement = document.getElementById("messages");
     //asserting that i know the objects will deff not be null with "!"
     messagesElement!.innerText = "Welcome to MultiMath! Starting a new game. . .";
+}
+
+function logPlayer(name: string) {
+    console.log(`New game starting for player: ${name}`)
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
